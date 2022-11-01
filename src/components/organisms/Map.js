ffbd8e57611a5a7c93bbf0059969c77d74cc70cf
박유경 <pyk0514@kumoh.ac.kr>
2022-11-01 10:10:00 +0900

    [chore] 지도 읍면동 단위 polygon 영역표시

--- src/components/organisms/Map.js
+++ src/components/organisms/Map.js
@@ -1,10 +1,58 @@
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+/*global kakao*/
+import { Map, MapMarker, Polygon, Rectangle } from "react-kakao-maps-sdk";
+import { useEffect, useState } from "react";
+import $ from "jquery";
 
 const GGSBMap = () => {
-    return (<Map
+    let [paths, setPaths] = useState([]);
+    // 파일 불러오기
+    useEffect(() => {
+        let pthtmp = [];
+        $.getJSON(`${process.env.PUBLIC_URL}/assets/TKmap.geojson`, function(geojson) {
+            var data = geojson.features;
+            var coordinates = [];
+            var name = '';
+
+            $.each(data, function(index, val) {
+                coordinates = val.geometry.coordinates;
+                name = val.properties.EMD_KOR_NM;
+                displayArea(coordinates, name);
+            })
+        })
+        // 도형 그리기
+        function displayArea(coordinates, name) {
+            var path = [];
+            var points = [];
+    
+            $.each(coordinates[0], function(idx, coord) {
+                $.each(coord, function(cdidx, cd) {
+                    var point = new Object();
+                    point.x = cd[1];
+                    point.y = cd[0];
+                    points.push(point);
+                    path.push({ lat: point.x, lng: point.y });
+                })
+            })
+            pthtmp.push(path);
+            setPaths(pthtmp);
+        }
+    }, []);
+
+
+    return (<Map 
         level={11}
-        center={{ lat: 35.85133, lng: 127.734086 }}
+        center={{lat: 36.45133, lng: 127.734086}}
         style={{ width: "100%", height: "1024px" }}>
+            {paths.length !== 0 ? paths.map((path) =>
+            <Polygon
+            path={path}
+            strokeWeight={2}
+            strokeColor={'#b26bb2'}
+            strokeOpacity={0.8}
+            fillColor={'#f9f'}
+            fillOpacity={0.7}
+            />
+            ) : <></>}
         </Map>);
 }
 
