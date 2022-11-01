f2e57373e000c9aa7dff22c477aca1056352fcf6
박유경 <pyk0514@kumoh.ac.kr>
2022-11-01 12:36:29 +0900

    [chore] 지도 클릭했을 때 중심에 오도록 수정
    영역 색깔 변하게 해야함

--- src/components/organisms/Map.js
+++ src/components/organisms/Map.js
@@ -5,9 +5,17 @@ import $ from "jquery";
 
 const GGSBMap = () => {
     let [paths, setPaths] = useState([]);
+    let [names, setNames] = useState([]);
+    let [centers, setCenters] = useState([]);
+    const [center, setCenter] = useState({lat: 36.45133, lng: 128.534086});
+    const [level, setLevel] = useState(11);
+    const geocoder = new kakao.maps.services.Geocoder();
     // 파일 불러오기
     useEffect(() => {
         let pthtmp = [];
+        let nmtmp = [];
+        let cttmp = [];
+
         $.getJSON(`${process.env.PUBLIC_URL}/assets/TKmap.geojson`, function(geojson) {
             var data = geojson.features;
             var coordinates = [];
@@ -15,8 +23,10 @@ const GGSBMap = () => {
 
             $.each(data, function(index, val) {
                 coordinates = val.geometry.coordinates;
-                name = val.properties.EMD_KOR_NM;
+                name = val.properties.EMD_NM;
                 displayArea(coordinates, name);
+                nmtmp.push(name);
+                setNames(nmtmp);
             })
         })
         // 도형 그리기
@@ -35,21 +45,73 @@ const GGSBMap = () => {
             })
             pthtmp.push(path);
             setPaths(pthtmp);
+
+            cttmp.push(centroid(points));
+            setCenters(cttmp);
         }
     }, []);
 
+    function centroid(points) {
+        var i, j, len, p1, p2, f, area, x, y;
+        area = x = y = 0;
 
-    return (<Map 
-        level={11}
-        center={{lat: 36.45133, lng: 128.534086}}
+        for(i=0, len=points.length, j=len-1; i<len; j=i++) {
+            p1 = points[i];
+            p2 = points[j];
+
+            f = p1.y * p2.x - p2.y * p1.x;
+            x += (p1.x + p2.x) * f;
+            y += (p1.y + p2.y) * f;
+            area += f * 3;
+        }
+        return { lat: x/area, lng: y/area };
+    }
+
+    var mouseOverOpt = {
+        fillColor: 'skyblue',
+        fillOpacity: 0.8
+    }
+
+    var mouseOutOpt = {
+        fillColor: 'gray',
+        fillOpacity: 0.8
+    }
+
+    var mouseClickOpt = {
+        fillColor: 'blue',
+        fillOpacity: 0.8
+    }
+
+    const onMouseOverEvt = (e) => {
+        e.setOptions(mouseOverOpt)
+    }
+
+    const onMouseOutEvt = (e) => {
+        e.setOptions(mouseOutOpt)
+    }
+    const onClickEvt = (e, idx) => {
+        setCenter(centers[idx]);
+        setLevel(level > 9 ? level-2 : level);
+        e.setOptions(mouseClickOpt);
+    }
+
+    return (<Map id="GGSBMap"
+        level={level}
+        onZoomChanged={(map) => {
+            setLevel(map.getLevel());
+        }}
+        center={center}
         style={{ width: "100%", height: "1024px" }}>
-            {paths.length !== 0 ? paths.map((path) =>
+            {paths.length !== 0 ? paths.map((path, idx) =>
             <Polygon
+            onMouseover={(e)=> onMouseOverEvt(e)}
+            onMouseout={(e)=> onMouseOutEvt(e)}
+            onClick={(e) => onClickEvt(e, idx)}
             path={path}
             strokeWeight={2}
-            strokeColor={'#b26bb2'}
+            strokeColor={'black'}
             strokeOpacity={0.8}
-            fillColor={'#f9f'}
+            fillColor={'gray'}
             fillOpacity={0.7}
             />
             ) : <></>}
