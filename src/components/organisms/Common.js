cd49297bae3c65aa37c17e0f4073bd2850c61e40
박유경 <pyk0514@kumoh.ac.kr>
2022-11-02 14:07:14 +0900

    [chore] 범위 축소로 인한 행정구역 selectbox 수정
    대구경북 -> 경북

--- src/components/organisms/Common.js
+++ src/components/organisms/Common.js
@@ -12,14 +12,13 @@ const Logo = () => {
 }
 
 const Search = () => {
-    const state = [{name: '대구광역시', value: 0}, {name: '경상북도', value: 1}];
     const city = [{name: '경산시', value: 0}, {name: '구미시', value: 1}];
     const district = [{name: '거의동', value: 0}, {name: '옥계동', value: 1}];
     return (
     <ColFlex id="SearchArea">
         <P>수질이 궁금한 지역을 검색해주세요 ex) 경상북도 구미시 거의동</P>
         <RowFlex id="Search">
-            <SelectBox label="행정구역" items={state}></SelectBox>
+            <SelectBox label="경상북도" items={[]}></SelectBox>
             <SelectBox label="시/군/구" items={city}></SelectBox>
             <SelectBox label="읍/면/동" items={district}></SelectBox>
             <SearchBtn />
